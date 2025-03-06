de4673c1d520d06afa71d3e60b033066126760d7
Kam Chan <kamchan@stanford.edu>
2025-03-06 11:35:00 -0800

    Initial bluecore terraform

---
+++ s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "kambuckethere1" {
+  bucket = "kambuckethere1"
+}
---
+++ variables-env.tf
@@ -0,0 +1,8 @@
+variable "users_account_id" {
+  description = "Account ID for current environment"
+  default     = "038462753403"
+}
+
+variable "profile" {
+  description = "User profile to use when running"
+}
